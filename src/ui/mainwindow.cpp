8b63f36487db964e69ffc9a35a814a7d456a4dc0
Evgeny Neustroev <neugen86@gmail.com>
2016-12-10 20:07:26 +0300

    First worked version

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <scene/scene.h>
+#include <widget/qsimulatorwidget.h>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QSharedPointer<scene::Scene> pScene = QSharedPointer<scene::Scene>(new scene::Scene);
+
+    widget::QSimulatorWidget* pWdg = new widget::QSimulatorWidget(pScene, pScene, this);
+    setCentralWidget(pWdg);
+
+    pScene->start();
+
+    pScene->insertObject(physics::Object(algebra::Point(256., 192.), 5.));
+    pScene->insertObject(physics::Object(algebra::Point(512., 567.), 7.5));
+    pScene->insertObject(physics::Object(algebra::Point(768., 384.), 10.));
 }
 
 MainWindow::~MainWindow()
